cc4e1307de7ee8f5b40511ff5253083a2a1f2767
TBOOTHE20 <tboothe20@gmail.com>
2016-11-26 00:50:51 -0500

    CSC-285/PhotoGallery-TatianaBoothe

--- app/src/main/java/com/bignerdranch/android/photogallery/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/SingleFragmentActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.graphics.drawable.RippleDrawable;
+//import android.graphics.drawable.RippleDrawable;
 
 
 public abstract class SingleFragmentActivity extends AppCompatActivity
